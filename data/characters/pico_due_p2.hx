e703c8ed3d1c77b9f4fd5b50492f032a7e4769de
MAN <147908955+babyMan32@users.noreply.github.com>
2026-06-27 03:32:42 -0400

    blast

--- data/characters/pico_due_p2.hx
+++ data/characters/pico_due_p2.hx
@@ -1,5 +1,15 @@
 var allow_attack = true;
 var killed_yellow = false;
+var killed_jor = false;
+var threatening;
+
+function onCreatePost()
+{
+	if (boyfriend.curCharacter == 'pico_due_p2')
+	{
+		threatening = FlxG.sound.load(Paths.sound('hankshoot', null, PathsTestMode.LOOSE));
+	}
+}
 
 function onUpdate(elapsed:Float):Void
 {
@@ -11,12 +21,16 @@ function onUpdate(elapsed:Float):Void
 
 function onUpdatePost(elapsed:Float):Void
 {
-	if (inCutscene || cpuControlled) return;
-
 	if (controls.NOTE_TAUNT_P && boyfriend.curCharacter == 'pico_due_p2' && allow_attack && !game.startingSong)
 	{
+		threatening.play(true);
 		boyfriend.playAnim('gunblast', true);
-		boyfriend.specialAnim = boyfriend.holding = true;
+		boyfriend.specialAnim = true;
+
+		if (!cpuControlled)
+		{
+			boyfriend.holding = true;
+		}
 
 		if (!killed_yellow)
 		{
@@ -31,8 +45,20 @@ function onUpdatePost(elapsed:Float):Void
 			dad.playAnim('death', true);
 			dad.specialAnim = killed_yellow = true;
 
-			setSongTime(125 * 1005);
+			setSongTime(125.64 * 1000);
 			clearNotesBefore(Conductor.songPosition);
+		}
+
+		if (dad.curCharacter == 'jorsawsee' && PlayState.SONG.song == 'O2' && !killed_jor)
+		{
+			killed_jor = true;
+
+			setSongTime(45.37 * 1000);
+			clearNotesBefore(Conductor.songPosition);
+
+			camGame.alpha = 0;
+
+			new FlxTimer().start(0.85, function(_) camGame.alpha = 1);
 		}
 
 		allow_attack = false;
