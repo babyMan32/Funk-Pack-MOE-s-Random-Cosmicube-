60dd584ba57ec2cd6ae99ae2b018856bc2e83f93
MAN <147908955+babyMan32@users.noreply.github.com>
2026-07-06 19:32:26 -0400

    funny little rare chance

--- data/characters/ghostJames.hx
+++ data/characters/ghostJames.hx
@@ -1,4 +1,12 @@
 var baseY;
+var hahaRareChance = false;
+
+function onLoad()
+{
+	if (boyfriend.curCharacter != 'ghostJames') return;
+
+	healthLoss = 0;
+}
 
 function onCreatePost()
 {
@@ -6,6 +14,14 @@ function onCreatePost()
 
 	baseY = boyfriend.y;
 	camGame.alpha = 0;
+
+	hahaRareChance = FlxG.random.bool(10);
+
+	if (hahaRareChance)
+	{
+		boyfriend.stunned = true;
+		boyfriend.canTaunt = false;
+	}
 }
 
 function onUpdatePost(elapsed:Float):Void
@@ -27,4 +43,12 @@ function onSongStart()
 	if (boyfriend.curCharacter != 'ghostJames') return;
 
 	FlxTween.tween(camGame, {alpha: 1}, 1.5, {ease: FlxEase.sineOut});
+}
+
+function onBeatHit()
+{
+	if (hahaRareChance && curBeat % 2 == 0)
+	{
+		boyfriend.playAnim('idle', true);
+	}
 }
