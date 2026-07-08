d59ce42cce4cd88bcf338110cb3a547b838fd31c
MAN <147908955+babyMan32@users.noreply.github.com>
2026-07-08 18:06:15 -0400

    kade engine core

--- songs/double-trouble-v3/extraCode.hx
+++ songs/double-trouble-v3/extraCode.hx
@@ -1,12 +1,38 @@
+var camMovedToPlay = false;
+var camMovedToOpp = false;
+
 function onLoad()
 {
 	hasColor = false;
+
+	playHUD.scoreTxt.x += 50;
+	playHUD.scoreTxt.y += 15;
 }
 
 function onCreatePost()
 {
 	allowIconSwitching = false;
 	playHUD.iconP2.changeIcon(oppIconDuo);
-	playHUD.healthBar.setColors(hpColorDuo);
+	playHUD.healthBar.setColors(0xFFfd0101, 0xFF6cf83e);
 	pauseOverride = pauseDuo;
+}
+
+function onMoveCamera(focus)
+{
+	if (focus == 'boyfriend' && !camMovedToPlay)
+	{
+		dad.playAnim('idle', true);
+		gf.playAnim('idle', true);
+
+		camMovedToPlay = true;
+		camMovedToOpp = false;
+	}
+
+	if (focus == 'dad' && !camMovedToOpp)
+	{
+		boyfriend.playAnim('idle', true);
+
+		camMovedToPlay = false;
+		camMovedToOpp = true;
+	}
 }
