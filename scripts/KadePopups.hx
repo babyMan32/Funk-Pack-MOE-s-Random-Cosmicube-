aeb06ed6df6d2c18ec8d6c5f34909eebfb3a5269
MAN <147908955+babyMan32@users.noreply.github.com>
2026-07-11 18:52:10 -0400

    so many tweaks

--- scripts/KadePopups.hx
+++ scripts/KadePopups.hx
@@ -1,16 +1,62 @@
+var variable_epics = 0;
+var variable_sicks = 0;
+var variable_goods = 0;
+var variable_bads = 0;
+var variable_shits = 0;
+
 function goodNoteHit(note)
 {
 	if (!init_kade_hud) return;
 
-	holySmackerel = new FlxSprite(550, 150).loadGraphic(Paths.image('ui/v3/epic', null, null, PathsTestMode.LOOSE));
-	holySmackerel.scale.set(0.6, 0.6);
-	add(holySmackerel);
+	if (note.isSustainNote) return;
 
-	FlxTween.tween(holySmackerel, {y: holySmackerel.y - 100}, 2, {ease: FlxEase.quartOut, onComplete: function() ratsGone()});
+	FlxG.signals.postUpdate.addOnce(function() {
+		rating_to_spawn = bullshitFunc();
+
+		var holySmackerel = new FlxSprite(550, 150).loadGraphic(Paths.image('ui/v3/$rating_to_spawn', null, null, PathsTestMode.LOOSE));
+		holySmackerel.scale.set(0.7, 0.7);
+		add(holySmackerel);
+
+		FlxTween.tween(holySmackerel, {y: holySmackerel.y - 100}, 1, {ease: FlxEase.quadOut});
+
+		FlxTween.tween(holySmackerel, {x: holySmackerel.x + FlxG.random.int(-170, 170)}, 3, {ease: FlxEase.quadIn});
+
+		FlxTween.tween(holySmackerel, {y: 2500}, 2, {ease: FlxEase.quadIn, startDelay: 1});
+		FlxTween.tween(holySmackerel, {alpha: 0}, 1, {ease: FlxEase.quadIn, startDelay: 1, onComplete: function() holySmackerel.destroy()});
+	});
 }
 
-function ratsGone()
+function bullshitFunc()
 {
-	FlxTween.tween(holySmackerel, {y: 2500}, 2, {ease: FlxEase.quartIn});
-	FlxTween.tween(holySmackerel, {alpha: 0}, 1, {ease: FlxEase.quartIn});
+	if (epics > variable_epics)
+	{
+		variable_epics = epics;
+		return 'epic';
+	}
+
+	if (sicks > variable_sicks)
+	{
+		variable_sicks = sicks;
+		return 'sick';
+	}
+
+	if (goods > variable_goods)
+	{
+		variable_goods = goods;
+		return 'good';
+	}
+
+	if (bads > variable_bads)
+	{
+		variable_bads = bads;
+		return 'bad';
+	}
+
+	if (shits > variable_shits)
+	{
+		variable_shits = shits;
+		return 'shit';
+	}
+
+	return 'epic';
 }
