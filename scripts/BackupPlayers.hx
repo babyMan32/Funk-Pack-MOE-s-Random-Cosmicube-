2312f47d0538697d623e3315c27b35dab0276464
MAN <147908955+babyMan32@users.noreply.github.com>
2026-07-21 15:49:55 -0400

    updates to be done

--- scripts/BackupPlayers.hx
+++ scripts/BackupPlayers.hx
@@ -10,6 +10,8 @@ public var tauntAnim = 'hey';
 
 var direction = 'Left';
 
+var bf2StageRim;
+
 function onCreatePost()
 {
 	extraPlayer = ClientPrefs.equipment.get('extraPlayerSkin');
@@ -24,13 +26,80 @@ function onCreatePost()
 	bf2Icon = new HealthIcon(bf2.healthIcon, true);
 	playHUD.insert(2, bf2Icon);
 
-	if (boyfriend.shader == null) return;
+	shadersCheck();
+}
 
-	FlxG.signals.postUpdate.addOnce(function() {
-		bf2Rim = new funkin.game.shaders.ExtraDropShadowShader().copyFrom(boyfriend.shader);
-		bf2Rim.attachedSprite = bf2;
-		bf2.useRenderTexture = true;
-	});
+function shadersCheck()
+{
+	if (bf2 != null && bf2.getFlag('backlit') != true)
+	{
+		if (ClientPrefs.shaders)
+		{
+			switch (PlayState.SONG.stage)
+			{
+				case "defeat":
+					bf2StageRim = new funkin.game.shaders.ExtraDropShadowShader();
+		
+					bf2StageRim.threshold = .05;
+					bf2StageRim.strength = .85;
+					bf2StageRim.setColorMatrix([
+						.4, .5, -.2, 0, -50,
+						-.25, .7, -.15, 0, -20,
+						.42, -.35, .85, 0, -72,
+						0, 0, 0, 1, 0
+					]);
+					bf2StageRim.addLayer([
+						.7, .5, 1, 0, 192,
+						.3, .4, -.5, 0, 64,
+						-.1, .2, .35, 0, 74,
+						0, 0, 0, 1, 0
+					], 10, 14, .01);
+					bf2StageRim.addLayer(
+						bf2StageRim.addLayer([
+							.9, .6, .4, 0, 4,
+							-.2, .5, .1, 0, -18,
+							-.2, .2, .4, 0, -28,
+							0, 0, 0, 1, 0
+						], 12, 40, .01, .4)
+					.colorMatrix, 96, 24, .01, .4);
+
+				case "reactor":
+					bf2StageRim = new funkin.game.shaders.ExtraDropShadowShader();
+		
+					bf2StageRim.threshold = .05;
+					bf2StageRim.setColorMatrix([
+						.8, .1, .2, 0, -40,
+						0, .35, .1, 0, 2,
+						.15, .12, .56, 0, -5,
+						0, 0, 0, 1, 0
+					]);
+					bf2StageRim.addLayer([
+						1, .3, 0, 0, 125,
+						.1, 1, 0, 0, 114,
+						-.1, -.1, 1, 0, 80,
+						0, 0, 0, 1, 0
+					], 120, 20, .05);
+					bf2StageRim.addLayer(
+						bf2StageRim.addLayer([
+							.8, .2, .2, 0, 14,
+							-.05, .6, 0, 0, 12,
+							-.1, .5, .81, 0, -20,
+							0, 0, 0, 1, 0
+						], 95, 38, .05)
+					.colorMatrix, 140, 32, .05);
+			}
+
+			if (bf2StageRim == null)
+			{
+				bf2.shader = boyfriend.shader;
+				return;
+			}
+
+			bf2Rim = bf2StageRim;
+			bf2Rim.attachedSprite = bf2;
+			bf2.useRenderTexture = true;
+		}
+	}
 }
 
 function onBeatHit()
@@ -99,6 +168,8 @@ function onUpdatePost(elapsed:Float):Void
 	bf2Icon.scale.set(iconP1.scale.x, iconP1.scale.y);
 	bf2Icon.shader = iconP1.shader;
 	bf2Icon.updateIconAnim(healthBar.percent * 0.01);
+	bf2Icon.alpha = iconP1.alpha;
+	bf2Icon.visible = iconP1.visible;
 
 	if (cpuControlled) return;
 
--- scripts/MoreBackupPlayers.hx
+++ scripts/MoreBackupPlayers.hx
@@ -10,6 +10,8 @@ public var tauntAnim2 = 'hey';
 
 var direction = 'Left';
 
+var bf3StageRim;
+
 function onCreatePost()
 {
 	extraExtraPlayer = ClientPrefs.equipment.get('extraExtraPlayerSkin');
@@ -19,18 +21,85 @@ function onCreatePost()
 	bf3 = new Character(0, 0, extraExtraPlayer, true);
 	boyfriendGroup.insert(3, bf3);
 	bf3.x += bf3.getFlag('offsetX') ?? -250;
-	bf3.y += bf3.getFlag('offsetY') ?? 75;
+	bf3.y += bf3.getFlag('offsetY') ?? 225;
 
 	bf3Icon = new HealthIcon(bf3.healthIcon, true);
 	playHUD.insert(5, bf3Icon);
 
-	if (boyfriend.shader == null) return;
+	shadersCheck();
+}
 
-	FlxG.signals.postUpdate.addOnce(function() {
-		bf3Rim = new funkin.game.shaders.ExtraDropShadowShader().copyFrom(boyfriend.shader);
-		bf3Rim.attachedSprite = bf3;
-		bf3.useRenderTexture = true;
-	});
+function shadersCheck()
+{
+	if (bf3 != null && bf3.getFlag('backlit') != true)
+	{
+		if (ClientPrefs.shaders)
+		{
+			switch (PlayState.SONG.stage)
+			{
+				case "defeat":
+					bf3StageRim = new funkin.game.shaders.ExtraDropShadowShader();
+		
+					bf3StageRim.threshold = .05;
+					bf3StageRim.strength = .85;
+					bf3StageRim.setColorMatrix([
+						.4, .5, -.2, 0, -50,
+						-.25, .7, -.15, 0, -20,
+						.42, -.35, .85, 0, -72,
+						0, 0, 0, 1, 0
+					]);
+					bf3StageRim.addLayer([
+						.7, .5, 1, 0, 192,
+						.3, .4, -.5, 0, 64,
+						-.1, .2, .35, 0, 74,
+						0, 0, 0, 1, 0
+					], 10, 14, .01);
+					bf3StageRim.addLayer(
+						bf3StageRim.addLayer([
+							.9, .6, .4, 0, 4,
+							-.2, .5, .1, 0, -18,
+							-.2, .2, .4, 0, -28,
+							0, 0, 0, 1, 0
+						], 12, 40, .01, .4)
+					.colorMatrix, 96, 24, .01, .4);
+
+				case "reactor":
+					bf3StageRim = new funkin.game.shaders.ExtraDropShadowShader();
+		
+					bf3StageRim.threshold = .05;
+					bf3StageRim.setColorMatrix([
+						.8, .1, .2, 0, -40,
+						0, .35, .1, 0, 2,
+						.15, .12, .56, 0, -5,
+						0, 0, 0, 1, 0
+					]);
+					bf3StageRim.addLayer([
+						1, .3, 0, 0, 125,
+						.1, 1, 0, 0, 114,
+						-.1, -.1, 1, 0, 80,
+						0, 0, 0, 1, 0
+					], 120, 20, .05);
+					bf3StageRim.addLayer(
+						bf3StageRim.addLayer([
+							.8, .2, .2, 0, 14,
+							-.05, .6, 0, 0, 12,
+							-.1, .5, .81, 0, -20,
+							0, 0, 0, 1, 0
+						], 95, 38, .05)
+					.colorMatrix, 140, 32, .05);
+			}
+
+			if (bf3StageRim == null)
+			{
+				bf3.shader = boyfriend.shader;
+				return;
+			}
+
+			bf3Rim = bf3StageRim;
+			bf3Rim.attachedSprite = bf3;
+			bf3.useRenderTexture = true;
+		}
+	}
 }
 
 function onBeatHit()
@@ -99,6 +168,8 @@ function onUpdatePost(elapsed:Float):Void
 	bf3Icon.scale.set(iconP1.scale.x, iconP1.scale.y);
 	bf3Icon.shader = iconP1.shader;
 	bf3Icon.updateIconAnim(healthBar.percent * 0.01);
+	bf3Icon.alpha = iconP1.alpha;
+	bf3Icon.visible = iconP1.visible;
 
 	if (cpuControlled) return;
 
