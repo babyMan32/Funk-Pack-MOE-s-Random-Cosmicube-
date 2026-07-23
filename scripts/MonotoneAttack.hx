5eace65da82b6e995b2ec60c47c25726bc82a587
MAN <147908955+babyMan32@users.noreply.github.com>
2026-07-23 18:36:53 -0400

    monotone attack thing fixed

---
+++ scripts/MonotoneAttack.hx
@@ -0,0 +1,47 @@
+function onLoad()
+{
+	if (ClientPrefs.bfSkin == "clowfoe-old")
+	{
+		switch (PlayState.SONG.song)
+		{
+			case "Monotone Attack":
+				PlayState.SONG.player1 = 'clowfoe-old';
+				PlayState.SONG.player2 = 'attack-old';
+				PlayState.SONG.gfVersion = 'fabs-old';
+		}
+	}
+}
+
+function onCreatePost()
+{
+	hellyeah();
+}
+
+function hellyeah()
+{
+	if (ClientPrefs.bfSkin == "clowfoe-old")
+	{
+		switch (PlayState.SONG.song)
+		{
+			case "Monotone Attack":
+				if (PlayState.attackCharacter > 1)
+				{
+					FlxG.signals.postUpdate.addOnce(function() {
+						healthBar.setColors(gf.healthColour, '-3217');
+					});
+				}
+
+				if (PlayState.attackCharacter == 1 || PlayState.attackCharacter == 2)
+				{
+					healthBar.leftToRight = true;
+
+					switch (PlayState.attackCharacter)
+					{
+						case 1:
+							iconP1.changeIcon('attack_OLD');
+							iconP2.changeIcon('bfclow_OLD');
+					}
+				}
+		}
+	}
+}
