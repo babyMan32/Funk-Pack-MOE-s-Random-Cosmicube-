d705af82b358f27de2607086ba966e1377b6384e
MAN <147908955+babyMan32@users.noreply.github.com>
2026-07-23 23:26:09 -0400

    many small optimizations

---
+++ data/characters/bf-defeat-normal-dialogue.hx
@@ -0,0 +1,18 @@
+function onEvent(eventName, value1, value2)
+{
+	switch (eventName)
+	{
+		case 'Defeat Fade':
+			var charType:Int = Std.parseInt(value1);
+			if (Math.isNaN(charType)) charType = 0;
+
+			switch (charType)
+			{
+				case 0:
+					if (boyfriend.curCharacter == 'bf-defeat-normal-dialogue')
+					{
+						changeCharacter('bf-defeat-scared-dialogue', 0);
+					}
+			}
+	}
+}
