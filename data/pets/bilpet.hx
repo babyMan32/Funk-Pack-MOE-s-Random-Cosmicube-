84e37a7644afbdc3e8b978de76601c4e87441c91
MAN <147908955+babyMan32@users.noreply.github.com>
2026-07-24 18:39:32 -0400

    bullshit go

--- data/pets/bilpet.hx
+++ data/pets/bilpet.hx
@@ -2,23 +2,27 @@ var beeg = false;
 
 function onLoad() 
 {
-    if (FlxG.random.bool(10)) 
-    {
-        pet.scale.set(1.2, 1.2);
-        pet.y -= 450;
-        pet.x -= 200;
-        pet.updateHitbox();
-        beeg = true;
-    }
+	playbackRate = FlxG.random.float(0.8, 1.2);
+
+	if (FlxG.random.bool(10)) 
+	{
+		pet.scale.set(1.2, 1.2);
+		pet.y -= 450;
+		pet.x -= 200;
+		pet.updateHitbox();
+		beeg = true;
+
+		playbackRate *= 1.2;
+	}
 }
 
 function onCreatePost()
 {
-    if (beeg && PlayState.SONG.song == 'Identity Crisis')
-    {
-        copyPet.scale.set(1.2, 1.2);
-        copyPet.y = pet.y - 25;
-        copyPet.x -= 200;
-        copyPet.updateHitbox();
-    }
+	if (beeg && PlayState.SONG.song == 'Identity Crisis')
+	{
+		copyPet.scale.set(1.2, 1.2);
+		copyPet.y = pet.y - 25;
+		copyPet.x -= 200;
+		copyPet.updateHitbox();
+	}
 }
