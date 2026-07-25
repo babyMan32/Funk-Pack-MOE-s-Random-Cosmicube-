6fefc86929194c1e2b4a15c2b99a0120c0a6961b
MAN <147908955+babyMan32@users.noreply.github.com>
2026-07-25 03:17:07 -0400

    i need the state to pop up later fucker

--- scripts/plugins/DrippypopInject.hx
+++ scripts/plugins/DrippypopInject.hx
@@ -1,35 +1,55 @@
 import funkin.states.FreeplayState;
 
+var game;
+
+var song;
+
+var select_song_mouse;
+
 function onUpdate()
 {
-	var game = FlxG.state;
+	game = FlxG.state;
 
 	var select_song = Controls.instance.ACCEPT;
-
-	var overlappingCard = (FlxG.mouse.y >= 319) && (FlxG.mouse.y <= 424);
   
 	if (Std.isOfType(game, FreeplayState))
 	{
-		var select_song_mouse = FlxG.mouse.justPressed && FlxG.mouse.overlaps(game.cards) && overlappingCard;
-
 		if (game.ws_lock[FreeplayState.curSelect]) return;
+
+		song = game.week_songs[FreeplayState.curSelect];
 
 		if (select_song || select_song_mouse)
 		{
-			var song = game.week_songs[FreeplayState.curSelect];
-      
-			if (song[0] == 'Drippypop' && !game.lockMovement)
-			{
-				game.lockMovement = true;
-				Paths.overrideMode = PathsTestMode.LOOSE; // i was going insane trying to load substates not from legacy, thank you ashley
-				game.openSubState(new ScriptedSubstate('DrippyGlungSubState'));
-				Paths.overrideMode = null;
-				return;
-			}
+			if (song[0] != 'Drippypop') return;
+			
+			if (game.lockMovement) return;
+
+			game.lockMovement = true;
+			Paths.overrideMode = PathsTestMode.LOOSE; // i was going insane trying to load substates not from legacy, thank you ashley
+			game.openSubState(new ScriptedSubstate('DrippyGlungSubState'));
+			Paths.overrideMode = null;
+			return;
 		}
 	}
 }
 
+function onUpdatePost()
+{
+	if (Std.isOfType(game, FreeplayState))
+	{
+		select_song_mouse = FlxG.mouse.y >= (game.upperBar.y + game.upperBar.height) && FlxG.mouse.justPressed && FlxG.mouse.overlaps(game.cards);
+	}
+}
+
 // plugin scirpts dont have onUpdate rn so  this will do i guess??
-function onLoad() FlxG.signals.preUpdate.add(onUpdate);
-function onDestroy() FlxG.signals.preUpdate.remove(onUpdate);
+function onLoad()
+{
+	FlxG.signals.preUpdate.add(onUpdate);
+	FlxG.signals.postUpdate.add(onUpdatePost);
+}
+
+function onDestroy()
+{
+	FlxG.signals.preUpdate.remove(onUpdate);
+	FlxG.signals.postUpdate.remove(onUpdatePost);
+}
