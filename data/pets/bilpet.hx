cb29678ff4eaf9079a7ea9a96d2469f926839725
MAN <147908955+babyMan32@users.noreply.github.com>
2026-07-27 17:35:36 -0400

    billa

--- data/pets/bilpet.hx
+++ data/pets/bilpet.hx
@@ -1,4 +1,4 @@
-var beeg = false;
+var state = 'norm';
 
 var speed = 1;
 
@@ -7,12 +7,23 @@ function onLoad()
 	if (FlxG.random.bool(10)) 
 	{
 		pet.scale.set(1.2, 1.2);
-		pet.y -= 450;
 		pet.x -= 200;
+		pet.y -= 450;
 		pet.updateHitbox();
-		beeg = true;
+		state = 'beeg';
 
 		playbackRate = Math.round(playbackRate * 120) / 100;
+	}
+
+	if (FlxG.random.bool(10) && state != 'beeg') 
+	{
+		pet.scale.set(0.1, 0.1);
+		pet.x += 100;
+		pet.y += 150;
+		pet.updateHitbox();
+		state = 'smol';
+
+		playbackRate = Math.round(playbackRate * 80) / 100;
 	}
 
 	speed = playbackRate;
@@ -22,14 +33,24 @@ function onCreatePost()
 {
 	if (PlayState.SONG.song != 'Identity Crisis') return;
 
-	if (beeg)
+	if (state == 'beeg')
 	{
 		copyPet.scale.set(1.2, 1.2);
-		copyPet.y = pet.y - 25;
-		copyPet.x -= 200;
+		copyPet.x -= 190;
+		copyPet.y = pet.y - 42;
 		copyPet.updateHitbox();
 
 		playbackRate = Math.round(playbackRate * 120) / 100;
+	}
+
+	if (state == 'smol')
+	{
+		copyPet.scale.set(0.1, 0.1);
+		copyPet.x += 85;
+		copyPet.y = pet.y + 42;
+		copyPet.updateHitbox();
+
+		playbackRate = Math.round(playbackRate * 80) / 100;
 	}
 
 	speed = playbackRate;
