f1722b8e69f8ab287b1bdcb8eee3a0055964047d
kludge <105128686+kluwudge@users.noreply.github.com>
2026-07-27 19:03:56 -0300

    change window icon!!

--- songs/double-trouble-v3/extraCode.hx
+++ songs/double-trouble-v3/extraCode.hx
@@ -1,4 +1,6 @@
 import cpp.Windows;
+import lime.graphics.Image;
+import funkin.FunkinAssets;
 
 var dev_store;
 var fps_store;
@@ -21,6 +23,10 @@ function onLoad()
 
 	FlxG.stage.window.title = 'FNF: VS Impostor'; //haha the window title changed lmfao
 	Windows.setDarkMode(false); //become blind bitch
+
+	var img:Bytes = FunkinAssets.getBytes(Paths.getPath('images/icon16-v3.png', null, PathsTestMode.LOOSE));
+	var icon:Image = Image.fromBytes(img);
+	FlxG.stage.window.setIcon(icon);
 }
 
 function onStartCountdown()
@@ -54,6 +60,10 @@ function onDestroy()
 
 	FlxG.stage.window.title = 'VS IMPOSTOR LEGACY v' + Main.LEGACY_VERSION;
 	Windows.setDarkMode();
+
+	var img:Bytes = FunkinAssets.getBytes(Paths.getPath('images/icon16-legacy.png', null, PathsTestMode.LOOSE));
+	var icon:Image = Image.fromBytes(img);
+	FlxG.stage.window.setIcon(icon);
 }
 
 function onMoveCamera(focus)
