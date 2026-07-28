b23d89cb11b5c03d75228ccfc59843c42c705964
Totally Not Idks Stuff <grnbro66@gmail.com>
2026-07-28 17:35:41 -0400

    Right Finger finally fucking works LES GO-

--- songs/right-finger/script.hx
+++ songs/right-finger/script.hx
@@ -1,9 +1,23 @@
 function onCreatePost() {
     skipCountdown = true;
 
-    var logo:FlxSprite = new FlxSprite(0, 0).loadGraphic(Paths.image('onetimeuselol/wight_ringer_titlecard'));
+    logo = new FlxSprite(0, 0).loadGraphic(Paths.image('onetimeuselol/wight_ringer_titlecard'));
     logo.camera = camHUD;
     logo.scale.set(0, 0);
     logo.screenCenter();
     playHUD.add(logo);
+}
+
+function onEvent(ev, v1, v2)
+{
+	if (ev == 'Legacy')
+	{
+		switch (v1)
+		{
+			case 'renderin':
+				FlxTween.tween(logo, {"scale.x": 0.75, "scale.y": 0.75}, 1.75, {startDelay: 0.25, ease: FlxEase.quartOut});
+            case 'renderout':
+                FlxTween.tween(logo, {"scale.x": 0, "scale.y": 0}, 1.25, {startDelay: 1, ease: FlxEase.quintIn});
+        }
+    }
 }
