964b8b67f4164c022f80c33bc9dba71160c97ad6
MAN <147908955+babyMan32@users.noreply.github.com>
2026-08-01 01:01:58 -0400

    changed at least one file i think

--- scripts/BullshitSkinsLmao.hx
+++ scripts/BullshitSkinsLmao.hx
@@ -2,6 +2,7 @@ import funkin.data.NoteSkin;
 
 var skins;
 var skinsOpp;
+var lastSkins;
 
 function onCreatePost()
 {
@@ -52,4 +53,17 @@ function onEvent(eventName, value1, value2)
 			triggerEventNote('Change Noteskin', skins, 0);
 			triggerEventNote('Change Noteskin', skinsOpp, 1);
 	}
+}
+
+function onUpdatePost(elapsed:Float):Void
+{
+	if (!boyfriend.hasFlag('botplayNoteSkin')) return; // mainly only used for the MMV2 chars
+
+	skins = (cpuControlled ? boyfriend.getFlag('botplayNoteSkin') : (boyfriend.getFlag('noteSkin') ?? PlayState.SONG.arrowSkins[0]));
+
+	if (skins == lastSkins) return;
+
+	triggerEventNote('Change Noteskin', skins, 0);
+
+	lastSkins = skins;
 }
