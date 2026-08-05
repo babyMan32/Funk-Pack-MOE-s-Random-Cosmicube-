de671d04a2709307a3823ce766b9c57d4395d995
MAN <147908955+babyMan32@users.noreply.github.com>
2026-08-05 14:47:30 -0400

    beerfriend is so much better now

--- data/characters/beerfriend.hx
+++ data/characters/beerfriend.hx
@@ -1,10 +1,38 @@
+import openfl.filters.ShaderFilter;
+
+var beer;
+var drunk;
+
+var addedShaders = false;
+
+function onCreatePost()
+{
+	beer = newShader('lsd');
+	drunk = newShader('vhs');
+
+	if (camGame.filters == null)
+	{
+		camGame.filters = [];
+	}
+
+	if (camHUD.filters == null)
+	{
+		camHUD.filters = [];
+	}
+
+	if (camOther.filters == null)
+	{
+		camOther.filters = [];
+	}
+}
+
 function onUpdatePost(elapsed:Float):Void
 {
 	if (boyfriend.curCharacter != 'beerfriend') return;
 
 	if (health >= 1.6)
 	{
-		playHUD.healthBar.setColors((dad.curCharacter == 'beerfriend-shimny' ? 0x67009b : dad.healthColour), FlxColor.WHITE);
+		playHUD.healthBar.setColors((dad.curCharacter == 'beerfriend-shimny' ? 0x67009b : null), FlxColor.WHITE);
 		playHUD.iconP1.animation.curAnim.curFrame = 2;
 
 		if (dad.curCharacter == 'beerfriend-shimny')
@@ -14,7 +42,7 @@ function onUpdatePost(elapsed:Float):Void
 	}
 	else if (health < 1.6 && health > 0.4)
 	{
-		playHUD.healthBar.setColors(dad.healthColour, boyfriend.healthColour);
+		playHUD.healthBar.setColors(null, boyfriend.healthColour);
 		playHUD.iconP1.animation.curAnim.curFrame = 0;
 
 		if (dad.curCharacter == 'beerfriend-shimny')
@@ -24,7 +52,7 @@ function onUpdatePost(elapsed:Float):Void
 	}
 	else
 	{
-		playHUD.healthBar.setColors((dad.curCharacter == 'beerfriend-shimny' ? FlxColor.PURPLE : dad.healthColour), 0x701e4d);
+		playHUD.healthBar.setColors((dad.curCharacter == 'beerfriend-shimny' ? FlxColor.PURPLE : null), 0x701e4d);
 		playHUD.iconP1.animation.curAnim.curFrame = 1;
 
 		if (dad.curCharacter == 'beerfriend-shimny')
@@ -37,11 +65,38 @@ function onUpdatePost(elapsed:Float):Void
 function opponentNoteHit(note)
 {
 	audio.pitch = 1 + (dad.curCharacter == 'beerfriend-shimny' ? FlxG.random.float(-0.5, 0.5) : 0);
+	FlxG.timeScale = 1;
+
+	camGame.filters = [];
+	camHUD.filters = [];
+	camOther.filters = [];
+
+	addedShaders = false;
 }
 
 function goodNoteHit(note)
 {
 	if (boyfriend.curCharacter != 'beerfriend') return;
 
-	audio.pitch = 1 + FlxG.random.float(-0.5, 0.5);
+	FlxG.signals.postUpdate.addOnce(function() {
+		audio.pitch = 1 + FlxG.random.float(-0.5, 0.5);
+
+		shaderShit();
+	});
+}
+
+function shaderShit()
+{
+	if (addedShaders) return;
+
+	addedShaders = true;
+
+	camGame.filters.push(new ShaderFilter(beer));
+	camGame.filters.push(new ShaderFilter(drunk));
+
+	camHUD.filters.push(new ShaderFilter(beer));
+	camHUD.filters.push(new ShaderFilter(drunk));
+
+	camOther.filters.push(new ShaderFilter(beer));
+	camOther.filters.push(new ShaderFilter(drunk));
 }
