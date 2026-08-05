c10ec6d12896efe1d611fa02ef55a6deab00b6a0
MAN <147908955+babyMan32@users.noreply.github.com>
2026-08-04 23:17:28 -0400

    beer pitch

--- data/characters/beerfriend.hx
+++ data/characters/beerfriend.hx
@@ -1,20 +1,47 @@
 function onUpdatePost(elapsed:Float):Void
 {
+	if (boyfriend.curCharacter != 'beerfriend') return;
+
 	if (health >= 1.6)
 	{
-		playHUD.healthBar.setColors(dad.healthColour, FlxColor.WHITE);
+		playHUD.healthBar.setColors((dad.curCharacter == 'beerfriend-shimny' ? 0x67009b : dad.healthColour), FlxColor.WHITE);
 		playHUD.iconP1.animation.curAnim.curFrame = 2;
-		playHUD.iconP1.flipX = true;
+
+		if (dad.curCharacter == 'beerfriend-shimny')
+		{
+			playHUD.iconP2.animation.curAnim.curFrame = 1;
+		}
 	}
 	else if (health < 1.6 && health > 0.4)
 	{
 		playHUD.healthBar.setColors(dad.healthColour, boyfriend.healthColour);
 		playHUD.iconP1.animation.curAnim.curFrame = 0;
-		playHUD.iconP1.flipX = false;
+
+		if (dad.curCharacter == 'beerfriend-shimny')
+		{
+			playHUD.iconP2.animation.curAnim.curFrame = 0;
+		}
 	}
 	else
 	{
-		playHUD.healthBar.setColors(dad.healthColour, 0x701e4d);
+		playHUD.healthBar.setColors((dad.curCharacter == 'beerfriend-shimny' ? FlxColor.PURPLE : dad.healthColour), 0x701e4d);
 		playHUD.iconP1.animation.curAnim.curFrame = 1;
+
+		if (dad.curCharacter == 'beerfriend-shimny')
+		{
+			playHUD.iconP2.animation.curAnim.curFrame = 2;
+		}
 	}
+}
+
+function opponentNoteHit(note)
+{
+	audio.pitch = 1 + (dad.curCharacter == 'beerfriend-shimny' ? FlxG.random.float(-0.5, 0.5) : 0);
+}
+
+function goodNoteHit(note)
+{
+	if (boyfriend.curCharacter != 'beerfriend') return;
+
+	audio.pitch = 1 + FlxG.random.float(-0.5, 0.5);
 }
