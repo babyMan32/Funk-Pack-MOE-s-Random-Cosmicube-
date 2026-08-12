fc8d39310659841a0983ebf3107ac079613aedcb
MAN <147908955+babyMan32@users.noreply.github.com>
2026-08-12 17:28:44 -0400

    jklfklsdndsfdsfdsfdsklfdsjkfdsjklfdsjklfdsjkfdslfdsjklfdsjklfdfkfdsklkfdjkfdsjklfdsjklfdsjkldsjklfdsjkfdsjklskdsldkljsklfs

--- songs/galfriend-attack/chars.hx
+++ songs/galfriend-attack/chars.hx
@@ -2,9 +2,12 @@ public var kaity:Character;
 
 var hittingNotes = false;
 
+var legallyDistinctMissCombo = 0;
+
 function onCreatePost()
 {
-	camSpecialThing([350, 550], [750, 550]);
+	canFollow = false;
+	camSpecialThing([650, 550], [650, 550], 0.7);
 
 	mom = new Character(0, 0, 'galfriend-opponent');
 	add(mom);
@@ -26,41 +29,34 @@ function onCreatePost()
 
 	for (i in opponentStrums) i.visible = false;
 
-	comboX = 440;
-
-	Paths.overrideMode = null;
-
-	vSliceScore = new FlxText(0, 0, 1280, (!cpuControlled ? "Score: 0" : "Botplay Enabled"));
-	vSliceScore.setFormat(Paths.font("vcr.ttf", false), 16, FlxColor.WHITE, 0, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-	vSliceScore.alignment = 'right'; //why were you so hard to figure out
-	vSliceScore.visible = false;
-	vSliceScore.x = playHUD.scoreTxt.x - 350;
-	vSliceScore.y = playHUD.scoreTxt.y + (ClientPrefs.downScroll ? -125 : 5);
-	playHUD.add(vSliceScore);
+	comboX = 400;
 }
 
 function onSpawnNote(note)
 {
 	if (note.lane != 0)
 	{
-		note.ignoreNote = true;
+		note.visible = false;
 		note.noAnimation = true;
 	}
+
+	note.setCustomColor([0xf9393f /*red channel*/, 0xffffff /*green channel*/, 0x651038 /*blue channel*/]);
 
 	if (note.noteType != 'Opponent Two' && note.noteType != 'Dad Sing' && note.noteType != 'Dad Sing Alt' && note.noteType != 'Kaity Sing') return;
 
 	note.noMissAnimation = true;
-}
 
-function onUpdatePost(elapsed:Float):Void
-{
-	if (cpuControlled)
+	switch (note.noteType)
 	{
-		vSliceScore.text = "Botplay Enabled";
-		return;
-	}
+		case 'Dad Sing', 'Dad Sing Alt':
+			note.setCustomColor([0xc24b99 /*red channel*/, 0xffffff /*green channel*/, 0x3c1f56 /*blue channel*/]);
 
-	vSliceScore.text = "Score: " + songScore;
+		case 'Opponent Two':
+			note.setCustomColor([0x00ffff /*red channel*/, 0xffffff /*green channel*/, 0x1542b7 /*blue channel*/]);
+
+		case 'Kaity Sing':
+			note.setCustomColor([0x12fa05 /*red channel*/, 0xffffff /*green channel*/, 0x0a4447 /*blue channel*/]);
+	}
 }
 
 function goodNoteHitPre(note:Note):Void
@@ -68,28 +64,30 @@ function goodNoteHitPre(note:Note):Void
 	final susMult:Float = (note.isSustainNote ? 1 / PlayState.instance.holdSubdivisions : 1);
 	final oppHealthGain:Float = (note.hitHealth * healthGain * susMult) * 2;
 
+	legallyDistinctMissCombo = 0;
+
 	hittingNotes = true;
 
 	switch (note.noteType)
 	{
 		case 'Opponent Two':
 			note.owner = mom;
-			if (!mustHitSection) camCurTarget = mom;
-			yesThisIsAFunctionIMade(mom, 'opponent', 'slice');
 			health -= oppHealthGain;
+			if (!mustHitSection) camCurTarget = mom;
+			yesThisIsAFunctionIMade(mom, 'opponent');
 
 		case 'Dad Sing':
 			note.owner = dad;
 			camCurTarget = null;
-			yesThisIsAFunctionIMade(dad, 'opponent', 'slice');
 			health -= oppHealthGain;
+			yesThisIsAFunctionIMade(dad, 'opponent');
 
 		case 'Dad Sing Alt':
 			note.owner = dad;
 			camCurTarget = null;
-			dad.animSuffix = '-alt';
-			yesThisIsAFunctionIMade(dad, 'opponent', 'slice');
 			health -= oppHealthGain;
+			note.animSuffix = '-alt';
+			yesThisIsAFunctionIMade(dad, 'opponent');
 
 		case 'Kaity Sing':
 			note.owner = kaity;
@@ -104,18 +102,22 @@ function goodNoteHitPre(note:Note):Void
 
 function noteMiss(note:Note):Void
 {
+	legallyDistinctMissCombo++;
+
 	switch (note.noteType)
 	{
 		case 'Opponent Two':
-			health += ((note.missHealth * healthLoss) * (PlayState.instance.missCombo + 1));
+			health += ((note.missHealth * healthLoss) * (legallyDistinctMissCombo + 1));
 			mom.playAnim(note.skin.singAnimations[note.noteData] + 'miss', true);
-			yesThisIsAFunctionIMade(mom, 'opponent', 'slice');
+			yesThisIsAFunctionIMade(mom, 'opponent');
+			PlayState.instance.missCombo = 0;
 			mom.holdTimer = 0;
 
 		case 'Dad Sing', 'Dad Sing Alt':
-			health += ((note.missHealth * healthLoss) * (PlayState.instance.missCombo + 1));
+			health += ((note.missHealth * healthLoss) * (legallyDistinctMissCombo + 1));
 			dad.playAnim(note.skin.singAnimations[note.noteData] + 'miss', true);
-			yesThisIsAFunctionIMade(dad, 'opponent', 'slice');
+			yesThisIsAFunctionIMade(dad, 'opponent');
+			PlayState.instance.missCombo = 0;
 			dad.holdTimer = 0;
 
 		case 'Kaity Sing':
@@ -137,24 +139,13 @@ function yesThisIsAFunctionIMade(character, position, ?engine = 'psych')
 	if (position == 'player')
 	{
 		playHUD.iconP1.changeIcon(character.healthIcon);
-		playHUD.healthBar.setColors(null, (engine != 'slice' ? character.healthColour : 0x66ff33));
+		playHUD.healthBar.setColors(null, character.healthColour);
 	}
 
 	if (position == 'opponent')
 	{
 		playHUD.iconP2.changeIcon(character.healthIcon);
-		playHUD.healthBar.setColors((engine != 'slice' ? character.healthColour : 0xff0000), null);
-	}
-
-	if (engine == 'slice')
-	{
-		playHUD.scoreTxt.visible = false;
-		vSliceScore.visible = true;
-	}
-	else
-	{
-		playHUD.scoreTxt.visible = true;
-		vSliceScore.visible = false;
+		playHUD.healthBar.setColors(character.healthColour, null);
 	}
 }
 
@@ -176,5 +167,42 @@ function onGameOver()
 	{
 		health = 0;
 		return Function_Stop;
+	}
+}
+
+function onEvent(eventName, value1, value2)
+{
+	switch (eventName)
+	{
+		case 'Legacy':
+			if (value1 != 'camShit') return;
+
+			canFollow = true;
+			defaultCamZoom = 0.9;
+
+			switch (value2)
+			{
+				case 'opp1':
+					camSpecialThing([350, 500], [350, 500]);
+
+				case 'opp2':
+					camSpecialThing([550, 600], [550, 600]);
+
+				case 'opps':
+					camSpecialThing([450, 550], [450, 550]);
+
+				case 'play1':
+					camSpecialThing([950, 500], [950, 500]);
+
+				case 'play2':
+					camSpecialThing([750, 600], [750, 600]);
+
+				case 'plays':
+					camSpecialThing([850, 550], [850, 550]);
+
+				case 'all':
+					canFollow = false;
+					camSpecialThing([650, 550], [650, 550], 0.7);
+			}
 	}
 }
