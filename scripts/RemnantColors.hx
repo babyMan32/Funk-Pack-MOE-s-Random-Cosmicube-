ac2e241a28d1b48a4ae9ff2317ff95a026d322c3
SammyTheGoog <sammytheman61@gmail.com>
2026-09-01 05:31:36 -0400

    Updated Remnants Shader Compatibility

--- scripts/RemnantColors.hx
+++ scripts/RemnantColors.hx
@@ -1,19 +1,45 @@
+import funkin.game.shaders.ExtraDropShadowShader;
+
 using StringTools;
 
 var remnanytsShader;
+var currentShader:FlxShader;
+var bfReal:Bool = false;
 
-function onCreatePost()
-{
-	if (!curSong.contains('Remnants')) return;
+function onCreatePost() {
+	if (!curSong.contains('Remnants'))
+		return;
 
-	if (boyfriend.curCharacter.contains('remnants')) return;
+	if (boyfriend != null && !boyfriend.curCharacter.contains('remnants')
+		&& !boyfriend.curCharacter.contains('ghost')
+		&& !boyfriend.curCharacter.contains('nightmare')
+		|| boyfriend.curCharacter == 'yellow-ghostPLUS') {
+		wRemnants(boyfriend);
+		boyfriend.ghostsEnabled = false;
+		bfReal = true;
+	}
 
-	wRemnants(boyfriend);
-	wRemnants(gf);
+	if (gf != null && !gf.curCharacter.contains('remnants')
+		&& !gf.curCharacter.contains('ghost')
+		&& !gf.curCharacter.contains('nightmare')) {
+		wRemnants(gf);
+		gf.ghostsEnabled = false;
+	}
+
+	if (hasPet && bfReal && !pet.curPet.contains('remnants')
+		&& !pet.curPet.contains('ghost')
+		&& !pet.curPet.contains('nightmare')) {
+		wRemnants(pet);
+
+		// TODO: Find a variable for the pet's cosmicube colors instad of just copying BF's health color
+		petShader = new ExtraDropShadowShader();
+		petShader.copyFrom(boyfriend.shader);
+		petShader.attachedSprite = pet;
+		pet.useRenderTexture = true;
+	}
 }
 
-function wRemnants(character)
-{
+function wRemnants(character) {
 	remnanytsShader = new funkin.game.shaders.ExtraDropShadowShader();
 
 	bullshitR = FlxColor.getRed(character.healthColour);
@@ -25,7 +51,7 @@ function wRemnants(character)
 		0, 0, 0, 0, bullshitR,
 		0, 0, 0, 0, bullshitG,
 		0, 0, 0, 0, bullshitB,
-		0, 0, 0, 1, 0
+		0, 0, 0, 1,         0
 	]);
 	remnanytsShader.setColorMatrix([
 		0, 0, 0, 0, 0,
