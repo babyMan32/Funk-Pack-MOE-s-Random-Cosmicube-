9199c88477122b01da5d515945c6e7d35ead0724
MAN <147908955+babyMan32@users.noreply.github.com>
2026-09-11 18:44:15 -0400

    updates based on my new pr, will undo if it doesnt get pulled

--- data/characters/bfMADNESSnew.hx
+++ data/characters/bfMADNESSnew.hx
@@ -7,9 +7,6 @@ function onCreatePost()
 			case "Danger":
 				triggerEventNote('Change Character', 'boyfriend', 'bf_demise_body');
 
-			case "Defeat":
-				triggerEventNote('Change Character', 'boyfriend', 'bfMADNESSexenew');
-
 			case "Finale":
 				triggerEventNote('Change Character', 'boyfriend', 'bfMADNESSexenewnervous');
 		}
@@ -42,32 +39,6 @@ function onEvent(eventName, value1, value2)
 {
 	switch (eventName)
 	{
-		case 'Defeat Retro':
-			var charType:Int = Std.parseInt(value1);
-			if (Math.isNaN(charType)) charType = 0;
-			
-			switch (charType)
-			{
-				case 1:
-					if (boyfriend.curCharacter == 'bfMADNESSnew')
-					{
-						triggerEventNote('Change Character', 'boyfriend', 'bfMADNESSexenewnervous');
-					}
-			}
-
-		case 'Defeat Fade':
-			var charType:Int = Std.parseInt(value1);
-			if (Math.isNaN(charType)) charType = 0;
-
-			switch (charType)
-			{
-				case 0:
-					if (boyfriend.curCharacter == 'bfMADNESSexenew')
-					{
-						triggerEventNote('Change Character', 'boyfriend', 'bfMADNESSexenewnervous');
-					}
-			}
-
 		case 'Legacy':
 			switch (value1)
 			{
