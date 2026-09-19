530a5253299daa1f94d2026e8e647a5cb07099f6
MAN <147908955+babyMan32@users.noreply.github.com>
2026-09-19 18:17:07 -0400

    updated PicoSkins script so it's easy to add more pico songs

--- scripts/PicoSkins.hx
+++ scripts/PicoSkins.hx
@@ -1,26 +1,30 @@
 public var _hell_yeah_picos = ['17bucks-pico', 'evil-impostor-pico', 'pick', 'pico_due_p1', 'pico_due_p2', 'pico-mix-player', 'pico-playable', 'piico'];
 
+public var _array_picos = [
+	['suspect', 'picoweird'],
+	['roomcode', 'picoroomcode']
+];
+
+var baseChar = 'pico';
+
+var leShitFuckAss;
+var leShitFuckAss2;
+
 function onLoad()
 {
 	songName = Paths.sanitize(PlayState.SONG.song);
 
+	for (i in 0..._array_picos.length)
+	{
+		leShitFuckAss = _array_picos[i][0];
+		leShitFuckAss2 = _array_picos[i][1];
+	}
+
 	switch (songName)
 	{
-		case 'suspect':
-			for (phillies in 0..._hell_yeah_picos.length)
-			{
-				if (ClientPrefs.bfSkin == _hell_yeah_picos[phillies])
-				{
-					PlayState.SONG.player1 = ClientPrefs.bfSkin;
-					return;
-				}
-				else
-				{
-					PlayState.SONG.player1 = 'picoweird';
-				}
-			}
+		case leShitFuckAss:
+			baseChar = leShitFuckAss2;
 
-		case 'roomcode':
 			for (phillies in 0..._hell_yeah_picos.length)
 			{
 				if (ClientPrefs.bfSkin == _hell_yeah_picos[phillies])
@@ -30,7 +34,7 @@ function onLoad()
 				}
 				else
 				{
-					PlayState.SONG.player1 = 'picoroomcode';
+					PlayState.SONG.player1 = baseChar;
 				}
 			}
 	}
